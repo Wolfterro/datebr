9b504e821ae78bd9a084fb5e547f8aefff3d750a
Wolfterro <wolfterrorista@hotmail.com>
2016-05-12 01:15:29 -0300

    Revertendo código. Algo deu errado...

--- datebr.c
+++ datebr.c
@@ -1,27 +1,3 @@
-/*
-* The MIT License (MIT)
-* 
-* Copyright (c) 2016 Wolfgang Almeida <wolfgang.almeida@yahoo.com>
-* 
-* Permission is hereby granted, free of charge, to any person obtaining a copy
-* of this software and associated documentation files (the "Software"), to deal
-* in the Software without restriction, including without limitation the rights
-* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-* copies of the Software, and to permit persons to whom the Software is
-* furnished to do so, subject to the following conditions:
-* 
-* The above copyright notice and this permission notice shall be included in all
-* copies or substantial portions of the Software.
-* 
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-* SOFTWARE.
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -51,9 +27,17 @@ int main(int argc, char * argv[]) {
 
 	// Declarações de variáveis
 	// ========================
-	char * dia_semana[6];
-	char * mes_nome[11];
-	char * horario_brasileiro[1];
+	int dia_semana_int;
+	int mes_nome_int;
+	int horario_brasileiro_int;
+	
+	int segundo_int;
+	int minuto_int;
+	int hora_int;
+
+	char dia_semana[16];
+	char mes_nome[16];
+	char horario_brasileiro[32];
 	
 	char segundo_corrigido[4];
 	char minuto_corrigido[4];
@@ -64,77 +48,138 @@ int main(int argc, char * argv[]) {
 
 	// Definindo dia da semana
 	// =======================
-	dia_semana[0] = "Domingo";
-	dia_semana[1] = "Segunda-Feira";
-	dia_semana[2] = "Terça-Feira";
-	dia_semana[3] = "Quarta-Feira";
-	dia_semana[4] = "Quinta-Feira";
-	dia_semana[5] = "Sexta-Feira";
-	dia_semana[6] = "Sábado";
-	
+	dia_semana_int = tm_tempo.tm_wday;
+	switch(dia_semana_int) {
+		case 0:
+			sscanf("Domingo", "%s", dia_semana);
+			break;
+		case 1:
+			sscanf("Segunda-Feira", "%s", dia_semana);
+			break;
+		case 2:
+			sscanf("Terça-Feira", "%s", dia_semana);
+			break;
+		case 3:
+			sscanf("Quarta-Feira", "%s", dia_semana);
+			break;
+		case 4:
+			sscanf("Quinta-Feira", "%s", dia_semana);
+			break;
+		case 5:
+			sscanf("Sexta-Feira", "%s", dia_semana);
+			break;
+		case 6:
+			sscanf("Sábado", "%s", dia_semana);
+			break;
+		default:
+			sscanf("N/D", "%s", dia_semana);
+			break;
+	}
+
 	// Definindo mês atual
 	// ===================
-	mes_nome[0] = "Janeiro";
-	mes_nome[1] = "Fevereiro";
-	mes_nome[2] = "Março";
-	mes_nome[3] = "Abril";
-	mes_nome[4] = "Maio";
-	mes_nome[5] = "Junho";
-	mes_nome[6] = "Julho";
-	mes_nome[7] = "Agosto";
-	mes_nome[8] = "Setembro";
-	mes_nome[9] = "Outubro";
-	mes_nome[10] = "Novembro";
-	mes_nome[11] = "Dezembro";
-	
+	mes_nome_int = tm_tempo.tm_mon;
+	switch(mes_nome_int) {
+		case 0:
+			sscanf("Janeiro", "%s", mes_nome);
+			break;
+		case 1:
+			sscanf("Fevereiro", "%s", mes_nome);
+			break;
+		case 2:
+			sscanf("Março", "%s", mes_nome);
+			break;
+		case 3:
+			sscanf("Abril", "%s", mes_nome);
+			break;
+		case 4:
+			sscanf("Maio", "%s", mes_nome);
+			break;
+		case 5:
+			sscanf("Junho", "%s", mes_nome);
+			break;
+		case 6:
+			sscanf("Julho", "%s", mes_nome);
+			break;
+		case 7:
+			sscanf("Agosto", "%s", mes_nome);
+			break;
+		case 8:
+			sscanf("Setembro", "%s", mes_nome);
+			break;
+		case 9:
+			sscanf("Outubro", "%s", mes_nome);
+			break;
+		case 10:
+			sscanf("Novembro", "%s", mes_nome);
+			break;
+		case 11:
+			sscanf("Dezembro", "%s", mes_nome);
+			break;
+		default:
+			sscanf("N/D", "%s", mes_nome);
+			break;
+	}
+
 	// Definindo tipo de horário
 	// =========================
-	horario_brasileiro[0] = "Horário Brasileiro";
-	horario_brasileiro[1] = "Horário Brasileiro de Verão";
-	
-	// Adicionando um zero a segundos menores que 10
-	// =============================================
-	if (tm_tempo.tm_sec < 10) {
-		sprintf(segundo_corrigido, "0%d", tm_tempo.tm_sec);
+	horario_brasileiro_int = tm_tempo.tm_isdst;
+	if (horario_brasileiro_int == 0) {
+		sscanf("Horário Brasileiro", "%[^\n]s", horario_brasileiro);
+	}
+	else if (horario_brasileiro_int < 0) {
+		sscanf("N/D", "%[^\n]s", horario_brasileiro);
 	}
 	else {
-		sprintf(segundo_corrigido, "%d", tm_tempo.tm_sec);
+		sscanf("Horário Brasileiro de Verão", "%[^\n]s", horario_brasileiro);
+	}
+
+	// Adicionando um zero a segundos menores que 10
+	// =============================================
+	segundo_int = tm_tempo.tm_sec;
+	if (segundo_int < 10) {
+		sprintf(segundo_corrigido, "0%d", segundo_int);
+	}
+	else {
+		sprintf(segundo_corrigido, "%d", segundo_int);
 	}
 
 	// Adicionando um zero a minutos menores que 10
 	// ============================================
-	if (tm_tempo.tm_min < 10) {
-		sprintf(minuto_corrigido, "0%d", tm_tempo.tm_min);
+	minuto_int = tm_tempo.tm_min;
+	if (minuto_int < 10) {
+		sprintf(minuto_corrigido, "0%d", minuto_int);
 	}
 	else {
-		sprintf(minuto_corrigido, "%d", tm_tempo.tm_min);
+		sprintf(minuto_corrigido, "%d", minuto_int);
 	}
 
 	// Adicionando um zero a horas menores que 10
 	// ==========================================
-	if (tm_tempo.tm_hour < 10) {
-		sprintf(hora_corrigida, "0%d", tm_tempo.tm_hour);
+	hora_int = tm_tempo.tm_hour;
+	if (hora_int < 10) {
+		sprintf(hora_corrigida, "0%d", hora_int);
 	}
 	else {
-		sprintf(hora_corrigida, "%d", tm_tempo.tm_hour);
+		sprintf(hora_corrigida, "%d", hora_int);
 	}
 
 	// Mostrando resultados na tela
 	// ============================
 	if (argv[1] == NULL) {
-		printf("%s, %d de %s de %d - %s:%s:%s - %s\n", dia_semana[tm_tempo.tm_wday], tm_tempo.tm_mday, mes_nome[tm_tempo.tm_mon], tm_tempo.tm_year + 1900, hora_corrigida, minuto_corrigido, segundo_corrigido, horario_brasileiro[tm_tempo.tm_isdst]);
+		printf("%s, %d de %s de %d - %s:%s:%s - %s\n", dia_semana, tm_tempo.tm_mday, mes_nome, tm_tempo.tm_year + 1900, hora_corrigida, minuto_corrigido, segundo_corrigido, horario_brasileiro);
 	}
 	else if (0 == strcmp(argv[1], "-h") || 0 == strcmp(argv[1], "--help")) {
 		help();
 	}
 	else if (0 == strcmp(argv[1], "-d") || 0 == strcmp(argv[1], "--date")) {
-		printf("%s, %d de %s de %d\n", dia_semana[tm_tempo.tm_wday], tm_tempo.tm_mday, mes_nome[tm_tempo.tm_mon], tm_tempo.tm_year + 1900);
+		printf("%s, %d de %s de %d\n", dia_semana, tm_tempo.tm_mday, mes_nome, tm_tempo.tm_year + 1900);
 	}
 	else if (0 == strcmp(argv[1], "-t") || 0 == strcmp(argv[1], "--time")) {
 		printf("%s:%s:%s\n", hora_corrigida, minuto_corrigido, segundo_corrigido);
 	}
 	else if (0 == strcmp(argv[1], "-dst") || 0 == strcmp(argv[1], "--daylight-saving-time")) {
-		printf("%s\n", horario_brasileiro[tm_tempo.tm_isdst]);
+		printf("%s\n", horario_brasileiro);
 	}
 	else {
 		printf("Argumento desconhecido! Use -h ou --help para ajuda!\n");
